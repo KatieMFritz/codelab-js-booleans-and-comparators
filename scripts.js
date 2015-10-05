804415808eb5a1fcba1e407e51c5225d5f150f0f
Katie Fritz <katie@katiemfritz.com>
2015-10-04 21:51:57 -0400

    stay branch complete

--- scripts.js
+++ scripts.js
@@ -4,12 +4,26 @@ var choice1 = prompt("The sun is setting. Do you stay here for the night, or lea
 
 if (choice1 === "STAY") {
   var choice2a = prompt("How many of your 6 ration packs do you eat before bed? [Type a number]")
-}
 
+  if (choice2a < 1) {
+    alert("You pass out from low blood sugar and a bear eats you and your six ration packs. Didn't do you much good, did it? THE END")
+  }
+  else if (choice2a >= 1 && choice2a < 4) {
+    alert("Good work on the moderation. You live another day. THE END")
+  }
+  else if (choice2a >= 4 && choice2a <=6) {
+    alert("Your ration packs are pretty big. You explode from eating so many at once. THE END")
+  }
+  else if (choice2a > 6) {
+    alert("Dude, you didn't even have that many ration packs. Disqualified! THE END")
+  }
+  else {
+    alert("You didn't follow directions. THE END")
+  }
+}
 else if (choice1 === "LEAVE") {
   var choice2b = prompt("How many miles will you walk tonight? [Type a number]")
 }
-
 else {
-  prompt("Please type STAY or LEAVE.")
+  alert("You didn't follow directions. THE END")
 }
