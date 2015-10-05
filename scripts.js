356a4d985253628f1c4ae5d40cf9a90bc9f7227d
Katie Fritz <katie@katiemfritz.com>
2015-10-04 21:58:46 -0400

    add leave options and final alert

--- scripts.js
+++ scripts.js
@@ -21,9 +21,28 @@ if (choice1 === "STAY") {
     alert("You didn't follow directions. THE END")
   }
 }
+
 else if (choice1 === "LEAVE") {
   var choice2b = prompt("How many miles will you walk tonight? [Type a number]")
+
+  if (choice2b < 0) {
+    alert("Smartass. Disqualified. THE END")
+  }
+  else if (choice2b >= 0 && choice2b <2) {
+    alert("Unfortunately, there were lots of monsters in your immediate vicinity and they killed you when you stopped to rest. THE END")
+  }
+  else if (choice2b >= 2 && choice2b <=15) {
+    alert("You were lucky and made it to a safe hiding spot to sleep through the daylight. THE END")
+  }
+  else if (choice2b > 15) {
+    alert("You drop dead from exhaustion. THE END")
+  }
+  else {
+    alert("You didn't follow directions. THE END")
+  }
 }
 else {
   alert("You didn't follow directions. THE END")
 }
+
+alert("Thanks for playing! Refresh the page to start over.")
